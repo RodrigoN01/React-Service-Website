bc3ba5503c0ef66c0cf27679d4bd17e54fe345e6
Rodrigo Novaes <rod_novaes@hotmail.com>
2020-11-04 17:21:47 +1300

    completed navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Wasssup</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' />
+      </Switch>
+    </Router>
   );
 }
 
